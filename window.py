da0545522c2ede3f9c8685c9b50a775a0012978c
Jesus <jdam5000@gmail.com>
2022-07-18 13:53:59 -0400

    Surface created

--- window.py
+++ window.py
@@ -1,8 +1,25 @@
-from turtle import color
+from sys import exit
+from turtle import Screen
 import pygame
+
 
 pygame.init()
 
-canvas = pygame.display.set_mode((300,500))
+canvas = pygame.display.set_mode((800,400))
+pygame.display.set_caption('Dinosour game')
+Clock = pygame.time.Clock()
 
+test_surface = pygame.Surface((400,200))
+test_surface.fill('Red')
+true_value = True
 
+while true_value: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    
+    Screen.blit
+    
+    pygame.display.update()
+    Clock.tick(60)
