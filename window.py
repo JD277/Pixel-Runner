8e992bb76a78454db4fd6a6113be131503a2592f
Jesus <jdam5000@gmail.com>
2022-07-19 10:35:04 -0400

    se añadio el background

--- window.py
+++ window.py
@@ -1,25 +1,32 @@
 from sys import exit
-from turtle import Screen
 import pygame
 
-
 pygame.init()
+# Para Desplegar el canvas
+canvas = pygame.display.set_mode((800, 400))
 
-canvas = pygame.display.set_mode((800,400))
+# Para nombrar la pantalla
 pygame.display.set_caption('Dinosour game')
+
+# Para colocar la cantidad de FPS por segundo
 Clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((400,200))
-test_surface.fill('Red')
+# Nueva superficie
+# test_surface = pygame.Surface((400, 200))
+# test_surface.fill('Red')
+Background = pygame.image.load('Graphics/fondo_contacto.png')
+Ground = pygame.image.load('Graphics/ground.png')
 true_value = True
 
-while true_value: 
+while true_value:
+    # Para cerrar la ventana
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    
-    Screen.blit
-    
+
+    # Para generar la superficie
+    canvas.blit(Background, (0, 0))
+    canvas.blit(Ground,(0,75))
     pygame.display.update()
     Clock.tick(60)
